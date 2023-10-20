eb2f12bae67d3b1e4ecf50914b6790af25d15a6f
백재원 <bjw1622@gmail.com>
2023-10-20 10:30:32 +0900

    feat : use ref

--- src/DiaryEditor.js
+++ src/DiaryEditor.js
@@ -1,6 +1,9 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const DiaryEditor = () => {
+  const authInput = useRef();
+  const contentInput = useRef();
+
   const [state, setState] = useState({
     author: "",
     content: "",
@@ -17,11 +20,11 @@ const DiaryEditor = () => {
 
   const handleSubmit = () => {
     if (state.author.length < 1) {
-      alert("작성자는 최소 1글자 이상 입력해주세요");
+      authInput.current.focus();
       return;
     }
     if (state.content.length < 5) {
-      alert("일기 본문은 최소 5자 이상 입력해주세요");
+      contentInput.current.focus();
       return;
     }
     alert("저장 성공");
@@ -31,6 +34,7 @@ const DiaryEditor = () => {
       <h2>오늘의 일기</h2>
       <div>
         <input
+          ref={authInput}
           name="author"
           value={state.author}
           onChange={handleChangeState}
@@ -38,6 +42,7 @@ const DiaryEditor = () => {
       </div>
       <div>
         <textarea
+          ref={contentInput}
           name="content"
           value={state.content}
           onChange={handleChangeState}
