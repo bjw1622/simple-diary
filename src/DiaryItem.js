e419ed104f7c501ee546a77784d0f2b252d30f59
백재원 <bjw1622@gmail.com>
2023-10-23 10:15:37 +0900

    feat : props호출

--- src/DiaryItem.js
+++ src/DiaryItem.js
@@ -1,13 +1,16 @@
 const DiaryItem = ({ diaryList }) => {
   return (
-    <div className="DiaryName">
-      <div key={diaryList.id}>
-        <div>작성자 : {diaryList.author}</div>
-        <div>일기 본문 : {diaryList.content}</div>
-        <div>감정 : {diaryList.emotion}</div>
-        <div>작성 시간 : {diaryList.created_date}</div>
+    <div className="DiaryItem">
+      <div className="info" key={diaryList.id}>
+        <span>
+          작성자 : {diaryList.author} | 감정 점수 : {diaryList.emotion}
+        </span>
         <br />
+        <span className="date">
+          {new Date(diaryList.created_date).toLocaleDateString()}
+        </span>
       </div>
+      <div className="content">{diaryList.content}</div>
     </div>
   );
 };
