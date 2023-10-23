338397b648cbb1ea8781633d675f180e6d65d612
백재원 <bjw1622@gmail.com>
2023-10-23 15:49:56 +0900

    feat : change set data

--- src/App.js
+++ src/App.js
@@ -3,37 +3,6 @@ import "./App.css";
 import DiaryList from "./DiaryList";
 import { useRef, useState } from "react";
 
-// const dummyList = [
-//   {
-//     id: 1,
-//     author: "백재원",
-//     content: "sdadad",
-//     created_date: new Date().getTime(),
-//     emotion: 1,
-//   },
-//   {
-//     id: 2,
-//     author: "백재원",
-//     content: "sdadad",
-//     created_date: new Date().getTime(),
-//     emotion: 1,
-//   },
-//   {
-//     id: 3,
-//     author: "백재원",
-//     content: "sdadad",
-//     created_date: new Date().getTime(),
-//     emotion: 1,
-//   },
-//   {
-//     id: 4,
-//     author: "백재원",
-//     content: "sdadad",
-//     created_date: new Date().getTime(),
-//     emotion: 1,
-//   },
-// ];
-
 function App() {
   const [data, setData] = useState([]);
   const dataId = useRef(0);
@@ -51,10 +20,28 @@ function App() {
     dataId.current += 1;
     setData([newItem, ...data]);
   };
+
+  const onRemove = (targetId) => {
+    const newDiarytList = data.filter((it) => {
+      return it.id !== targetId;
+    });
+    setData(newDiarytList);
+  };
+
+  const onEdit = (targetId, newContent) => {
+    const a = data.map((it) =>
+      it.id === targetId ? { ...data, content: newContent } : it
+    );
+    setData(
+      data.map((it) => {
+        return it.id === targetId ? { ...it, content: newContent } : it;
+      })
+    );
+  };
   return (
     <div className="App">
       <DiaryEditor onCreate={onCreate} />
-      <DiaryList diaryList={data} />
+      <DiaryList diaryList={data} onRemove={onRemove} onEdit={onEdit} />
     </div>
   );
 }
