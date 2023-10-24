d3c09d7747bf8aea930922b3c16443fa00b298a7
백재원 <bjw1622@gmail.com>
2023-10-24 15:45:14 +0900

    feat : useMemo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
 import DiaryEditor from "./DiaryEditor";
 import "./App.css";
 import DiaryList from "./DiaryList";
-import { useRef, useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 
-function App() {
+// https://jsonplaceholder.typicode.com/comments
+
+const App = () => {
   const [data, setData] = useState([]);
+
   const dataId = useRef(0);
 
-  // 새로운 일기를 추가하는 함수
+  const getData = async () => {
+    const res = await fetch(
+      "https://jsonplaceholder.typicode.com/comments"
+    ).then((res) => res.json());
+
+    const initData = res.slice(0, 20).map((it) => {
+      return {
+        author: it.email,
+        content: it.body,
+        emotion: Math.floor(Math.random() * 5) + 1,
+        created_date: new Date().getTime(),
+        id: dataId.current++,
+      };
+    });
+    setData(initData);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   const onCreate = (author, content, emotion) => {
     const created_date = new Date().getTime();
     const newItem = {
@@ -29,21 +52,39 @@ function App() {
   };
 
   const onEdit = (targetId, newContent) => {
-    const a = data.map((it) =>
-      it.id === targetId ? { ...data, content: newContent } : it
-    );
     setData(
       data.map((it) => {
         return it.id === targetId ? { ...it, content: newContent } : it;
       })
     );
   };
+
+  // useMemo는 Memoization
+  // 성능 향상을 위해서
+  const getDiaryAnalysis = useMemo(() => {
+    console.log("일기 분석 시작");
+
+    const goodCount = data.filter((it) => {
+      return it.emotion >= 3;
+    }).length;
+
+    const badCount = data.length - goodCount;
+    const goodRatio = (goodCount / data.length) * 100;
+
+    return { goodCount, badCount, goodRatio };
+  }, [data.length]);
+
+  const { goodCount, badCount, goodRatio } = getDiaryAnalysis;
   return (
     <div className="App">
       <DiaryEditor onCreate={onCreate} />
+      <div>전체 일기 : {data.length}</div>
+      <div>기분 좋은 일기 개수 : {goodCount}</div>
+      <div>기분 나쁜 일기 개수 : {badCount}</div>
+      <div>기분 좋은 일기 비율 : {goodRatio}</div>
       <DiaryList diaryList={data} onRemove={onRemove} onEdit={onEdit} />
     </div>
   );
-}
+};
 
 export default App;
